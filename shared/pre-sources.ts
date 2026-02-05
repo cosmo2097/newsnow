dc129382cf1265538d1d443d718c91df779b9f4c
cosmo2097 <zhangchao51@163.com>
2026-02-05 17:41:08 +0800

    fix: 将预设来源 `zfw` 更名为 `zww` 并更新其标题为“住温网”。

--- shared/pre-sources.ts
+++ shared/pre-sources.ts
@@ -699,8 +699,8 @@ export const originSources = {
         interval: Time.Common,
         home: "https://mp.weixin.qq.com/",
       },
-      zfw: {
-        title: "住房网",
+      zww: {
+        title: "住温网",
         type: "realtime",
         interval: Time.Common,
         home: "https://mp.weixin.qq.com/",
